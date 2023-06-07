1c0fa4ee99bdc6d84855f0f3e89f4ebc47e7856a
HarshXsharma <88335515+HarshXsharma@users.noreply.github.com>
2023-06-07 18:54:34 +0530

    1. Set Matrix Zeros

---
+++ SetMatrixZeros.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+void setZeros(vector<vector<int>> &matrix)
+{
+	// Write your code here.
+	bool firstrow = false, firstcol = false;
+	
+	for( int j = 0; j < matrix[0].size(); j++ )
+	{
+		if( matrix[0][j] == 0 )
+		{
+			firstrow = true;
+			break;
+		}
+		
+	}
+
+	for( int j = 0; j < matrix.size(); j++ )
+	{
+		if( matrix[j][0] == 0 ) 
+		{
+			firstcol = true;
+			break;
+		}
+	}
+
+	for( int i = 1; i < matrix.size(); i++ )
+	{
+		for( int j = 1; j < matrix[0].size(); j++ )
+		{
+			if( matrix[i][j] == 0 ) 
+			{
+				matrix[i][0] = 0;
+				matrix[0][j] = 0;
+			}
+		}
+	}
+
+	for( int i = 1; i < matrix.size(); i++ )
+	{
+		for( int j = 1; j < matrix[0].size(); j++ )
+		{
+			if( matrix[i][0] == 0 || matrix[0][j] == 0 )
+			matrix[i][j] = 0;
+		}
+			
+	}
+
+	if( firstcol )
+	{
+		for( int i = 0; i < matrix.size(); i++ )
+		matrix[i][0] = 0;
+	}
+
+	if( firstrow )
+	{
+		for( int j = 0; j < matrix[0].size(); j++ )
+		matrix[0][j] = 0;
+	}
+}
